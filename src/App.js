368436a59f4ff5a276a7542e682b413855d5a531
Damian <themrdamus@gmail.com>
2020-10-05 16:22:23 +0100

    testing numbers api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    text: "enter date",
+    error: "error"
+  }
+
+    handleDateChange = (e) => {
+      const value = e.target.value;
+      fetch(`http://numbersapi.com/${value}/year?json`)
+      .then(res => {
+        if(res.ok) {
+          return res
+        }
+        throw Error(res.status)
+      })
+      .then(res => res.json())
+      .then(data => this.setState({
+        text: "In this year: " + data.text
+      }))
+      .catch(err => {
+        this.setState({text: "There is a problem " + err})
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <input onChange={this.handleDateChange} type="text"/>
+        <p>{this.state.text}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
